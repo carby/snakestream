daf54055b1876fab92cc5c0a59b5abbdee58d753
Tommy Derngren <tommy.derngren@hiq.se>
2023-01-14 19:04:12 +0100

    Support for_each()

--- tests/test_main.py
+++ tests/test_main.py
@@ -288,6 +288,34 @@ async def test_collect_to_list() -> None:
     assert it == [4, 5]
 
 
+# FOR EACH
+@pytest.mark.asyncio
+async def test_for_each() -> None:
+    def incr_y(c) -> None:
+        c['y'] = 1
+
+    await stream(coords) \
+        .for_each(incr_y)
+
+    assert coords[0]['y'] == 1
+    assert coords[1]['y'] == 1
+    assert coords[2]['y'] == 1
+
+
+@pytest.mark.asyncio
+async def test_for_each_async() -> None:
+    async def async_incr_y(c) -> None:
+        await asyncio.sleep(0.01)
+        c['y'] = 1
+
+    await stream(coords) \
+        .for_each(async_incr_y)
+
+    assert coords[0]['y'] == 1
+    assert coords[1]['y'] == 1
+    assert coords[2]['y'] == 1
+
+
 # OTHER
 @pytest.mark.asyncio
 async def test_mixed_chain() -> None:
