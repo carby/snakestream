f84be018842546f62b53a31a6e11f7cc1dd27868
Tommy Derngren <tommy.derngren@hiq.se>
2023-01-15 01:57:28 +0100

    Support find_first()

--- tests/test_main.py
+++ tests/test_main.py
@@ -316,6 +316,47 @@ async def test_for_each_async() -> None:
     assert coords[2]['y'] == 1
 
 
+# FIND FIRST
+@pytest.mark.asyncio
+async def test_find_first() -> None:
+    counter = 0
+
+    def incr_counter(c):
+        nonlocal counter
+        counter += 1
+        return c
+
+    # when
+    it = await stream([1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6]) \
+        .map(incr_counter) \
+        .filter(lambda x: x == 6) \
+        .find_first()
+
+    # then
+    assert it == 6
+    assert counter == 6
+
+
+@pytest.mark.asyncio
+async def test_find_first_found_none() -> None:
+    counter = 0
+
+    def incr_counter(c):
+        nonlocal counter
+        counter += 1
+        return c
+
+    # when
+    it = await stream([1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6]) \
+        .map(incr_counter) \
+        .filter(lambda x: x == 100) \
+        .find_first()
+
+    # then
+    assert it is None
+    assert counter == 12
+
+
 # OTHER
 @pytest.mark.asyncio
 async def test_mixed_chain() -> None:
