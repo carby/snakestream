66c66a8c45f5c419e46b68919ba3e1737b5ccbbd
Tommy Derngren <tommy.derngren@hiq.se>
2023-09-02 09:59:19 +0200

    Move domain types to seperate module

---
+++ src/snakestream/type.py
@@ -0,0 +1,119 @@
+import abc
+from typing import Any, AsyncGenerator, AsyncIterable, Awaitable, Callable, Generator, Iterable, Optional, TypeVar, Union
+
+# 
+# Generic types
+#
+T = TypeVar('T')
+R = TypeVar('R')
+
+Streamable = Union[Iterable, AsyncIterable, Generator, AsyncGenerator]
+
+Predicate = Callable[[T], Union[bool, Awaitable[bool]]]
+
+# Intermediaries
+Filterer = Callable[[T], T]
+Mapper = Callable[[T], Optional[R]]
+FlatMapper = Callable[[Streamable], 'AbstractStream']
+Comparator = Callable[[T, T], Union[bool, Awaitable[bool]]]
+Consumer = Callable[[T], T]
+
+# Terminals
+Accumulator = Callable[[T, Union[T, R]], Union[T, R]]
+
+
+# 
+# Classes
+#
+class AbstractStream(metaclass=abc.ABCMeta):
+
+    '''
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (
+            hasattr(subclass, 'empty') and 
+            callable(subclass.empty) and 
+
+            hasattr(subclass, 'concat') and 
+            callable(subclass.concat) or 
+
+            NotImplemented
+        )
+    '''
+
+    @staticmethod
+    @abc.abstractclassmethod
+    def empty() -> 'AbstractStream':
+        raise NotImplementedError
+
+    
+    @staticmethod
+    @abc.abstractclassmethod
+    async def concat(a: 'AbstractStream', b: 'AbstractStream') -> 'AbstractStream':
+        raise NotImplementedError
+
+    # Intermediaries
+    @abc.abstractclassmethod
+    def filter(self, predicate: Predicate) -> 'AbstractStream':
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    def map(self, mapper: Mapper) -> 'AbstractStream':
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    def flat_map(self, flat_mapper: FlatMapper) -> 'AbstractStream':
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    def sorted(self, comparator: Optional[Comparator] = None, reverse=False) -> 'AbstractStream':
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    def distinct(self) -> 'AbstractStream':
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    def peek(self, consumer: Consumer) -> 'AbstractStream':
+        raise NotImplementedError
+
+    # Terminals
+    @abc.abstractclassmethod
+    def collect(self, collector: Callable) -> AsyncGenerator:
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    async def reduce(self, identity: Union[T, R], accumulator: Accumulator) -> Union[T, R]:
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    async def for_each(self, consumer: Callable[[T], Any]) -> None:
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    async def find_first(self) -> Optional[Any]:
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    async def max(self, comparator: Comparator) -> Optional[T]:
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    async def min(self, comparator: Comparator) -> Optional[T]:
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    async def all_match(self, predicate: Predicate) -> bool:
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    async def none_match(self, predicate: Predicate) -> bool:
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    async def any_match(self, predicate: Predicate) -> bool:
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    async def count(self) -> int:
+        raise NotImplementedError
