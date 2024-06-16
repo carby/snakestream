f696547de70283debb4940adb05bac225504ba02
Tommy Derngren <tommy.derngren@hiq.se>
2024-06-16 16:29:18 +0200

    Add a missign tests for find_any()

--- tests/test_find_any.py
+++ tests/test_find_any.py
@@ -41,3 +41,22 @@ async def test_find_any_found_none() -> None:
     # then
     assert it is None
     assert counter == 12
+
+
+@pytest.mark.asyncio
+async def test_find_any_empty_stream() -> None:
+    counter = 0
+
+    def incr_counter(c):
+        nonlocal counter
+        counter += 1
+        return c
+
+    # when
+    it = await Stream.of([]) \
+        .map(incr_counter) \
+        .find_any()
+
+    # then
+    assert it is None
+    assert counter == 0
